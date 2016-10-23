e546a229ea33038994db54c410b342a234213543
Joshua Koo <zz85nus@gmail.com>
2016-10-23 18:10:25 +0800

    Render something

--- node.js
+++ node.js
@@ -2,12 +2,26 @@ const BG = '#333';
 
 // Biz Logic
 
+class Bounds {
+    constructor(x, y, w, h) {
+        return this.set(x, y, w, h);
+    }
+    
+    set(x, y, w, h) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        return this;
+    }
+}
+
 class BNode {
   constructor(name, topo) {
     this.name = name;
     this.connects = new Set();
-    top.add(this);
-//     this.bounds = {x, y, w, h};
+    topo.add(this);
+    this.bounds = new Bounds();
   }
   
   connectTo(node) {
@@ -25,8 +39,10 @@ class Topo {
   }
   
   layout() {
-    for (let n of this.nodes) {
-      console.log(n);
+    var x = 0;
+    for (let node of this.nodes) {
+        x += 100;
+        node.bounds.set(x, 50, 60, 20);
     }
   }
 }
@@ -45,13 +61,22 @@ canvas.height = window.innerHeight;
 
 const ctx = canvas.getContext('2d');
 
-function render() {
+function render(topo) {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   
   ctx.fillStyle = BG;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
+  
+  for (let node of topo.nodes) {
+      ctx.fillStyle = 'blue';
+      const bounds = node.bounds;
+      ctx.fillRect(bounds.x, bounds.y, bounds.w, bounds.h);
+      
+      ctx.fillStyle = 'yellow';
+      ctx.fillText(node.name, bounds.x, bounds.y);
+  }
 }
 
-render();
+render(topo);
 
 document.body.appendChild(canvas);
